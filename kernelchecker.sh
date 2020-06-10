154d942a2bc3fb16adcacc2f20a00701171b340c
Charlie Root <darthferrett@gmail.com>
2020-06-10 09:08:52 -0500

    Add check for /boot/GENERIC image.

--- kernelchecker.sh
+++ kernelchecker.sh
@@ -9,3 +9,12 @@ then
 The current kernel is a $CURRENT_IDENT kernel instead of the desired $DESIRED_IDENT kernel. Please recompile the kernel.
 EOF
 fi
+
+if [ ! -d /boot/GENERIC ]
+then
+	mail -s "GENERIC kernel image missing" root <<EOF
+The GENERIC kernel image is missing from /boot. Please compile and install a GENERIC kernel to /boot/GENERIC.
+EOF
+fi
+
+
